bfa91bcf20606292e5d184115d0814c79e416a9e
mozisheng <809121829@qq.com>
2020-12-27 22:04:17 +0800

    自定义实现 useLocalStore

--- src/m-mobx-react-lite/index.js
+++ src/m-mobx-react-lite/index.js
@@ -4,4 +4,6 @@ export { observer } from "./observer";
 
 export { Observer } from  './ObserverComponent';
 
-export { useAsObservableSource } from './useAsObservableSource'
+export { useAsObservableSource } from './useAsObservableSource';
+
+export { useLocalStore } from './useLocalStore'
--- src/pages/UseLocalStore.js
+++ src/pages/UseLocalStore.js
@@ -1,40 +1,62 @@
 import React from "react";
 import {
-  useLocalStore,
-  useObserver,
+//   useLocalStore,
+//   useObserver,
 //   useAsObservableSource
 } from "mobx-react-lite";
 
-import {useAsObservableSource} from "../m-mobx-react-lite/";
+import {
+    Observer,
+    useLocalStore,
+    useAsObservableSource
+} from "../m-mobx-react-lite/";
 
 function UseLocalStore(props) {
     // useAsObservableSource hook 可以将一组值转换为具有稳定引用的可观察对象(每次从钩子返回相同的对象)
 
     // 使用 useAsObservableSource 将 props 转换成可观察的对象
-    const newProps = useAsObservableSource(props);
+    // const newProps = useAsObservableSource(props);
+    const newProps = {...props}
     // useLocalStore第一个参数是一个初始化函数，并且这个函数只会执行一次，并且在整个生命周期中都是有效的
-    const countStore = useLocalStore(() => ({
-        count: props.init === undefined ? 0 : props.init,
-        add() {
-        this.count = this.count + 1;
-        },
-        get emoji() {
-        return this.count % 2 ? "😜" : "🏃";
-        },
-        get specialNum() {
-        return newProps.init > -1 && newProps.init < 10
-            ? "0" + newProps.init
-            : newProps.init;
-        }
-    }));
+    const countStore = useLocalStore(
+        newProps => ({
+            count: props.init === undefined ? 0 : props.init,
+            add() {
+            this.count = this.count + 1;
+            },
+            get emoji() {
+            return this.count % 2 ? "😜" : "🏃";
+            },
+            get specialNum() {
+                console.log('==specialNum=')
+            return newProps.init > -1 && newProps.init < 10
+                ? "0" + newProps.init
+                : newProps.init;
+            }
+        }),
+        newProps
+    );
     // useObserver 将组件转换成响应式组件
-    return useObserver(() => (
-        <div className="border">
-        <h3>UseLocalStore</h3>
-        <button onClick={countStore.add}>count: {countStore.count}</button>
-        <p>{countStore.emoji}</p>
-        <p>{countStore.specialNum}</p>
-        </div>
-    ));
+    // return useObserver(() => (
+    //     <div className="border">
+    //     <h3>UseLocalStore</h3>
+    //     <button onClick={countStore.add}>count: {countStore.count}</button>
+    //     <p>{countStore.emoji}</p>
+    //     <p>{countStore.specialNum}</p>
+    //     </div>
+    // ));
+    // 实现更细微的控制
+    return (
+        <Observer>
+        {() => (
+            <div className="border">
+            <h3>UseLocalStore</h3>
+            <button onClick={countStore.add}>count: {countStore.count}</button>
+            <p>{countStore.emoji}</p>
+            <p>{countStore.specialNum}</p>
+            </div>
+        )}
+        </Observer>
+    );
 }
 export default UseLocalStore;
